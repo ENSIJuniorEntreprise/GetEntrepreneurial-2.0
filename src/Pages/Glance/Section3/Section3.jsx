d60f238633492588dd5762752aa50d7f394bfd70
selimketata <ketata.15.selim@gmail.com>
2025-02-01 16:46:07 +0100

    Updates before deploying

--- src/Pages/Glance/Section3/Section3.jsx
+++ src/Pages/Glance/Section3/Section3.jsx
@@ -1,23 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Section3.css";
-import video from './../../../Assets/juniorentreprise.mp4'
+import image from "./../../../Assets/junior.jpg"
 
 export default function Section3() {
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const handlePlay = () => {
+    setIsPlaying(true);
+  };
+
   return (
     <div className="section27-container">
-      {/* Lignes décoratives orange */}
+      {/* Lignes décoratives */}
       <div className="horizontal-line top-line"></div>
       <div className="horizontal-line bottom-line"></div>
       <div className="vertical-line left-line"></div>
       <div className="vertical-line right-line"></div>
 
-      {/* Conteneur de la vidéo */}
+      {/* Conteneur de la vidéo avec image */}
       <div className="video-wrapper">
-        <video
-          src={video}
-          controls
-          className="video-content"
-        ></video>
+        {!isPlaying ? (
+          <div className="video-thumbnail" onClick={handlePlay}>
+            <img
+              src={image} // Remplace par l'image capturée
+              alt="Thumbnail"
+              className="thumbnail-image"
+              width="100%"
+              height="auto"
+            />
+            <button className="play-button">▶</button>
+          </div>
+        ) : (
+          <iframe
+            className="video-content"
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/nbpSj222j0s?autoplay=1"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        )}
       </div>
     </div>
   );
